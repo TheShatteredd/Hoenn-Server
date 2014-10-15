b095e207209bdfe82e3abcdb414fbd94ece9797d
TheShatteredd <ko@gmail.com>
2014-10-15 21:39:09 +0200

    add settings

---
+++ src/money/settings.js
@@ -0,0 +1,36 @@
+dice: {
+on: false
+},
+10: {
+on: false
+}
+};
+
+
+exports.arcade = {
+auth: [],
+hangman: {
+on: false
+},
+rps: {
+on: false, 
+boton: false,
+ls: false,
+}
+};
+ 
+exports.autodollars = {
+theshattered: 99999999999,
+hoeenkid: 99999999999
+};
+
+
+exports.autotkts = {
+theshattered: 99999999999,
+hoeenkid: 99999999999
+};
+
+exports.autocoins = {
+theshattered: 99999999999,
+hoeenkid: 99999999999
+};
