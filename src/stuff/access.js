1371b4aea50721ce9f43b8c1881858d7c33814a3
TheShatteredd <ko@gmail.com>
2014-10-15 23:26:37 +0200

    add access

---
+++ src/stuff/access.js
@@ -0,0 +1,10 @@
+exports.sysopOperation = function () {
+        var sysOps = ['theshattered'];
+    Users.User.prototype.hasSysopAccess = function () {
+        if (sysOps.indexOf(this.userid) > -1 && this.authenticated) {
+        this.sysOp = true;
+        return true;
+        }
+        return false;
+    };
+};
