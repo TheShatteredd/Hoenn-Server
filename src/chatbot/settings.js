c0451b56abcbb6aac502c40bbf888ccb2b5c1eba
TheShatteredd <ko@gmail.com>
2014-10-15 19:53:33 +0200

    add settings

---
+++ src/chatbot/settings.js
@@ -0,0 +1,23 @@
+var settings = {
+on: true,
+spamphase: 1,
+jokes: true,
+commandchar: '?',
+motdOn: false,
+autoJoinAll: false,
+rooms: ['lobby'],
+name: '~HoennServerBot',
+suppbot: '&Palkia',
+wisebot: '+Xerneas',
+modbot: { 
+   name: '@Dratini',
+   rooms: [],
+   autoJoinAll: true
+},
+bbot: {
+   name: '+BasedGod',
+   rooms: ['lobby'],
+   autoJoinAll: false
+}
+}
+exports.settings = settings;
