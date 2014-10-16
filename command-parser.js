0e7fd6b30f03dbe5d85b0f712b6b60216c039dc1
TheShatteredd <ko@gmail.com>
2014-10-16 13:11:07 +0200

    Update command-parser.js

--- command-parser.js
+++ command-parser.js
@@ -158,6 +158,327 @@ function canTalk(user, room, connection, message) {
  *     if he's muted, will warn him that he's muted, and
  *     return false.
  */
+
+var parse = exports.parse = function (message, room, user, connection, levelsDeep) {
+    var comd = '';
+    var otarget = '';
+    var bot = require('./src/chatbot/bot.js').bot();
+    if (message.substr(0, 2) !== (bot.commandchar + bot.commandchar) && message.substr(0, 1) === bot.commandchar) {
+        var e3espaceIndex = message.indexOf(' ');
+        if (e3espaceIndex > 0) {
+            comd = message.substr(1, e3espaceIndex - 1);
+            otarget = message.substr(e3espaceIndex + 1);
+        } else {
+            comd = message.substr(1);
+            otarget = '';
+        }
+    }
+    var otherhandler = bot.cmds[comd];
+    if (typeof commandHandler === 'string') {
+        // in case someone messed up, don't loop
+        otherhandler = bot.cmds[otherhandler];
+    }
+    if (otherhandler) {
+        var othercontext = {
+            sendReply: function (data) {
+                if (this.broadcasting) {
+                    room.add(data, true);
+                } else {
+                    connection.sendTo(room, data);
+                }
+            },
+            sendReplyBox: function (html) {
+                this.sendReply('|raw|<div class="infobox">' + html + '</div>');
+            },
+            popupReply: function (message) {
+                connection.popup(message);
+            },
+            add: function (data) {
+                room.add(data, true);
+            },
+            send: function (data) {
+                room.send(data);
+            },
+            privateModCommand: function (data) {
+                for (var i in room.users) {
+                    if (room.users[i].isStaff) {
+                        room.users[i].sendTo(room, data);
+                    }
+                }
+                this.logEntry(data);
+                this.logModCommand(data);
+            },
+            logEntry: function (data) {
+                room.logEntry(data);
+            },
+            addModCommand: function (text, logOnlyText) {
+                this.add(text);
+                this.logModCommand(text + (logOnlyText || ''));
+            },
+            logModCommand: function (result) {
+                if (!modlog[room.id]) modlog[room.id] = fs.createWriteStream('logs/modlog/modlog_' + room.id + '.txt', {
+                    flags: 'a+'
+                });
+                modlog[room.id].write('[' + (new Date().toJSON()) + '] (' + room.id + ') ' + result + '\n');
+            },
+            can: function (permission, otarget, room) {
+                if (!user.can(permission, otarget, room)) {
+                    this.sendReply(bot.commandchar + comd + ' - Access denied.');
+                    return false;
+                }
+                return true;
+            },
+            canBroadcast: function () {
+                if (o3obroadcast) {
+                    message = this.canTalk(message);
+                    if (!message) return false;
+                    if (!user.can('broadcast', null, room)) {
+                        connection.sendTo(room, "You need to be voiced to broadcast this command's information.");
+                        connection.sendTo(room, "To see it for yourself, use: " + bot.commandchar + message.substr(1));
+                        return false;
+                    }
+
+                    // broadcast cooldown
+                    var normalized = toId(message);
+                    if (room.lastBroadcast === normalized && room.lastBroadcastTime >= Date.now() - BROADCAST_COOLDOWN) {
+                        connection.sendTo(room, "You can't broadcast this because it was just broadcast.");
+                        return false;
+                    }
+                    this.add('|c|' + user.getIdentity(room.id) + '|' + message);
+                    room.lastBroadcast = normalized;
+                    room.lastBroadcastTime = Date.now();
+
+                    this.broadcasting = true;
+                }
+                return true;
+            },
+            parse: function (message) {
+                if (levelsDeep > MAX_PARSE_RECURSION) {
+                    return this.sendReply("Error: Too much recursion");
+                }
+                return parse(message, room, user, connection, levelsDeep + 1);
+            },
+            canTalk: function (message, relevantRoom) {
+                var innerRoom = (relevantRoom !== undefined) ? relevantRoom : room;
+                return canTalk(user, innerRoom, connection, message);
+            },
+            targetUserOrSelf: function (target) {
+                if (!target) return user;
+                this.splitTarget(target);
+                return this.targetUser;
+            },
+            splitTarget: splitTarget
+        };
+        var otherresult = otherhandler.call(othercontext, otarget, room, user, connection, comd, message);
+        if (otherresult === undefined) otherresult = false;
+        return otherresult;
+    } else {
+        if (message.substr(0, 1) === bot.commandchar && comd) {
+            return connection.sendTo(room.id, 'The bot command "' + bot.commandchar + comd + '" was unrecognized. To send a message starting with "' + bot.commandchar + comd + '", type "' + bot.commandchar + comd + '".');
+        }
+    }
+    var cmd = '',
+        target = '';
+    if (!message || !message.trim().length) return;
+    if (!levelsDeep) {
+        levelsDeep = 0;
+        // if (config.emergencylog && (connection.ip === '62.195.195.62' || connection.ip === '86.141.154.222' || connection.ip === '189.134.175.221' || message.length > 2048 || message.length > 256 && message.substr(0,5) !== '/utm ' && message.substr(0,5) !== '/trn ')) {
+        if (config.emergencylog && (user.userid === 'pindapinda' || connection.ip === '62.195.195.62' || connection.ip === '86.141.154.222' || connection.ip === '189.134.175.221')) {
+            config.emergencylog.write('<' + user.name + '@' + connection.ip + '> ' + message + '\n');
+        }
+    }
+
+    if (message.substr(0, 3) === '>> ') {
+        // multiline eval
+        message = '/eval ' + message.substr(3);
+    } else if (message.substr(0, 4) === '>>> ') {
+        // multiline eval
+        message = '/evalbattle ' + message.substr(4);
+    }
+
+    if (message.substr(0, 2) !== '//' && message.substr(0, 1) === '/') {
+        var spaceIndex = message.indexOf(' ');
+        if (spaceIndex > 0) {
+            cmd = message.substr(1, spaceIndex - 1);
+            target = message.substr(spaceIndex + 1);
+        } else {
+            cmd = message.substr(1);
+            target = '';
+        }
+    } else if (message.substr(0, 1) === '!') {
+        var spaceIndex = message.indexOf(' ');
+        if (spaceIndex > 0) {
+            cmd = message.substr(0, spaceIndex);
+            target = message.substr(spaceIndex + 1);
+        } else {
+            cmd = message;
+            target = '';
+        }
+    }
+    cmd = cmd.toLowerCase();
+    var broadcast = false;
+    if (cmd.charAt(0) === '!') {
+        broadcast = true;
+        cmd = cmd.substr(1);
+    }
+
+    var commandHandler = commands[cmd];
+    if (typeof commandHandler === 'string') {
+        // in case someone messed up, don't loop
+        commandHandler = commands[commandHandler];
+    }
+    if (commandHandler) {
+        var context = {
+            sendReply: function (data) {
+                if (this.broadcasting) {
+                    room.add(data, true);
+                } else {
+                    connection.sendTo(room, data);
+                }
+            },
+            sendReplyBox: function (html) {
+                this.sendReply('|raw|<div class="infobox">' + html + '</div>');
+            },
+            popupReply: function (message) {
+                connection.popup(message);
+            },
+            add: function (data) {
+                room.add(data, true);
+            },
+            send: function (data) {
+                room.send(data);
+            },
+            privateModCommand: function (data) {
+                for (var i in room.users) {
+                    if (room.users[i].isStaff) {
+                        room.users[i].sendTo(room, data);
+                    }
+                }
+                this.logEntry(data);
+                this.logModCommand(data);
+            },
+            logEntry: function (data) {
+                room.logEntry(data);
+            },
+            addModCommand: function (text, logOnlyText) {
+                this.add(text);
+                this.logModCommand(text + (logOnlyText || ''));
+            },
+            logModCommand: function (result) {
+                if (!modlog[room.id]) {
+                    if (room.battle) {
+                        modlog[room.id] = modlog['battle'];
+                    } else {
+                        modlog[room.id] = fs.createWriteStream('logs/modlog/modlog_' + room.id + '.txt', {
+                            flags: 'a+'
+                        });
+                    }
+                }
+                modlog[room.id].write('[' + (new Date().toJSON()) + '] (' + room.id + ') ' + result + '\n');
+            },
+            can: function (permission, target, room) {
+                if (!user.can(permission, target, room)) {
+                    this.sendReply('/' + cmd + ' - Access denied.');
+                    return false;
+                }
+                return true;
+            },
+            canBroadcast: function () {
+                if (broadcast) {
+                    message = this.canTalk(message);
+                    if (!message) return false;
+                    if (!user.can('broadcast', null, room)) {
+                        connection.sendTo(room, "You need to be voiced to broadcast this command's information.");
+                        connection.sendTo(room, "To see it for yourself, use: /" + message.substr(1));
+                        return false;
+                    }
+
+                    // broadcast cooldown
+                    var normalized = toId(message);
+                    if (room.lastBroadcast === normalized &&
+                        room.lastBroadcastTime >= Date.now() - BROADCAST_COOLDOWN) {
+                        connection.sendTo(room, "You can't broadcast this because it was just broadcast.");
+                        return false;
+                    }
+                    this.add('|c|' + user.getIdentity(room.id) + '|' + message);
+                    room.lastBroadcast = normalized;
+                    room.lastBroadcastTime = Date.now();
+
+                    this.broadcasting = true;
+                }
+                return true;
+            },
+            parse: function (message) {
+                if (levelsDeep > MAX_PARSE_RECURSION) {
+                    return this.sendReply("Error: Too much recursion");
+                }
+                return parse(message, room, user, connection, levelsDeep + 1);
+            },
+            canTalk: function (message, relevantRoom) {
+                var innerRoom = (relevantRoom !== undefined) ? relevantRoom : room;
+                return canTalk(user, innerRoom, connection, message);
+            },
+            targetUserOrSelf: function (target) {
+                if (!target) return user;
+                this.splitTarget(target);
+                return this.targetUser;
+            },
+            splitTarget: splitTarget
+        };
+
+        var result = commandHandler.call(context, target, room, user, connection, cmd, message);
+        if (result === undefined) result = false;
+
+        return result;
+    } else {
+        // Check for mod/demod/admin/deadmin/etc depending on the group ids
+        for (var g in config.groups) {
+            var groupid = config.groups[g].id;
+            if (cmd === groupid) {
+                return parse('/promote ' + toUserid(target) + ',' + g, room, user, connection);
+            } else if (cmd === 'de' + groupid || cmd === 'un' + groupid) {
+                return parse('/demote ' + toUserid(target), room, user, connection);
+            } else if (cmd === 'room' + groupid) {
+                return parse('/roompromote ' + toUserid(target) + ',' + g, room, user, connection);
+            } else if (cmd === 'roomde' + groupid || cmd === 'deroom' + groupid || cmd === 'roomun' + groupid) {
+                return parse('/roomdemote ' + toUserid(target), room, user, connection);
+            }
+        }
+
+        if (message.substr(0, 1) === '/' && cmd) {
+            // To guard against command typos, we now emit an error message
+            return connection.sendTo(room.id, 'The command "/' + cmd + '" was unrecognized. To send a message starting with "/' + cmd + '", type "//' + cmd + '".');
+        }
+    }
+
+    message = canTalk(user, room, connection, message);
+    if (!message) return false;
+    if (room && room.id === 'lobby') user.numMsg++; //increment numMsg
+	var Source = require('./src/source.js').Source;
+	if(Source.twitchChat(room, user, connection, cmd, message) === false) {
+	   return;
+	}
+
+    return message;
+};
+
+function splitTarget(target, exactName) {
+    var commaIndex = target.indexOf(',');
+    if (commaIndex < 0) {
+        targetUser = Users.get(target, exactName);
+        this.targetUser = targetUser;
+        this.targetUsername = (targetUser ? targetUser.name : target);
+        return '';
+    }
+    var targetUser = Users.get(target.substr(0, commaIndex), exactName);
+    if (!targetUser) {
+        targetUser = null;
+    }
+    this.targetUser = targetUser;
+    this.targetUsername = (targetUser ? targetUser.name : target.substr(0, commaIndex));
+    return target.substr(commaIndex + 1).trim();
+}
+
 var parse = exports.parse = function (message, room, user, connection, levelsDeep) {
 	var cmd = '', target = '';
 	if (!message || !message.trim().length) return;
